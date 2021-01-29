fad42281235b38a351f63424d582dec63e6de505
katriel <katriel181920@gmail.com>
2021-01-29 16:41:43 -0500

    Get Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ButtonModule} from 'primeng/button';
+
+
 @NgModule({
   declarations: [
     AppComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    ButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+/*
+//themes
+  "primeng/resources/themes/saga-green/theme.css",
+  "primeng/resources/themes/vela-green/theme.css",
+*/
