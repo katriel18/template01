ad32b622f32ca937618fbab25a1fef149545d12a
Osti Katriel <katriel181920@gmail.com>
2021-01-30 13:22:18 -0500

    delete one style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,30 +6,16 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+
   title = 'template01';
 
-  cssUrl: string='';
-
   constructor(
-    public sanitizer: DomSanitizer,
+    
     ) { }
 
   ngOnInit(): void {
-   this.cssUrl = '/assets/saga-green.css';
+   
   }
 
-  changeStyle() {
-    this.cssUrl = (this.cssUrl === '/assets/saga-green.css') ? '/assets/vela-green.css' : '/assets/saga-green.css';
-  }
 
 }
-
-/*
-  //themes
-  1.Eliminar el theme de styles
-  "node_modules/primeng/resources/themes/saga-green/theme.css",
-  "node_modules/primeng/resources/themes/vela-green/theme.css",
-  2.Hacer uso del DomSanitizer
-  '/assets/saga-green.css'
-  '/assets/vela-green.css'
-*/
